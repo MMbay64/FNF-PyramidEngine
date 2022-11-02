a8990768649a4961e3f6ef2b739893ad0b3170d6
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-11-02 05:55:06 -0400

    Wip

---
+++ Source/CreditState.hx
@@ -0,0 +1,23 @@
+Package;
+
+Class CreditState [
+Public Static var CreditState:Array<dynamic> - [
+["Pyramid Engine Team"],
+["MMbay64",  'mmbay',  "Engine Developer"],
+[""],
+["Bit Engine Team"],
+["MyFnf",  'myfnf',  "Engine Developer"],
+["Memehoovy",  'meme', "Engine Member"],
+["Stefan[BETA]2008",  'STE',  "Engine Member"],
+["MasterOak",  'oak', "Engine Member"],
+["MMbay64",  'mmbay',    "Engine Member"],
+[""],
+["Funkin'crew"],
+["NinjaMuffin",  'ninja',  "Developer"],
+["PhantomArcade",  'phantom',  "Developer"],
+["Evilsc8tor",   'evil',  "Developer"],
+["KawaiiSprite",  'kawaii',  "Developer"]
+//I'ma change this later cuz I'm tired as fuck
+
+
+ 

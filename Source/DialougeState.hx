94e2519aa281ded6278a92b7d7a4df237c1c02f6
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-11-02 04:51:05 -0400

    do

---
+++ Source/DialougeState.hx
@@ -0,0 +1,13 @@
+package;
+
+class dialoguestate {
+ var dialogue1:array<string> = ["Pyramids","P"]
+}; 
+
+class actionstate [
+public static var action1:array<dynamic> - [
+                              ["Action 1","None"],
+                              ["Action 2","Scared"],
+                              ["Action 3","Pissed"],
+                              ["Action 3","Happy"]
+  ]
